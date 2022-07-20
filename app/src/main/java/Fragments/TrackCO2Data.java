b08e0402756c6d4e8bc2f14fa1aebebe95f35e0f
Jimant2 <jimantoniou2@gmail.com>
2022-07-20 08:37:15 +0200

    Implemented the line graph for the TrackHumidityData page. Might add some more customisation later on, but the main idea has been completed.

--- app/src/main/java/Fragments/TrackCO2Data.java
+++ app/src/main/java/Fragments/TrackCO2Data.java
@@ -28,55 +28,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TrackHumidityData#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TrackCO2Data extends Fragment {
 
     BarChart barChart;
-    List barEntries;
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public TrackCO2Data() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment TrackHumidityData.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static TrackCO2Data newInstance(String param1, String param2) {
-//        TrackCO2Data fragment = new TrackCO2Data();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -92,15 +47,11 @@ public class TrackCO2Data extends Fragment {
         entries.add(new BarEntry(2f, 14f,"Highest"));
 
 
-//        barEntries = new ArrayList<>();
-//        barEntries.add(new BarEntry(1f, 2));
-//        barEntries.add(new BarEntry(3f, 4));
-//        barEntries.add(new BarEntry(5f, 6));
-
         BarDataSet barDataSet = new BarDataSet(entries, "CO2");
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
         barDataSet.setValueTextColor(Color.BLUE);
         barDataSet.setValueTextSize(18f);
+
 
         ArrayList<String> barFactors = new ArrayList<>();
         barFactors.add("Recommended");
@@ -112,16 +63,21 @@ public class TrackCO2Data extends Fragment {
         xAxis.setGranularityEnabled(true);
         BarData data = new BarData(barDataSet);
         data.setBarWidth(0.9f); // set custom bar width
-       // data.setValueTextSize(12);
 
         Description description = new Description();
-        description.setText("All values in parts-per-million");
+        description.setText("All values in percentages");
+        description.setTextSize(15);
         barChart.setDescription(description);
+
+        //Setting up the borders
+        barChart.setDrawGridBackground(true);
+        barChart.setBorderWidth(1);
+        barChart.setDrawBorders(true);
 
         barChart.setData(data);
         barChart.setFitBars(true); // make the x-axis fit exactly all bars
         barChart.invalidate(); // refresh
-        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(barFactors));
+       // barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(barFactors));
 
         //BARCHART LEGEND
         Legend l = barChart.getLegend();
@@ -130,25 +86,19 @@ public class TrackCO2Data extends Fragment {
         l.setTextSize(12f);
         l.setTextColor(Color.BLACK);
         List<LegendEntry> lentries = new ArrayList<>();
+
         for (int i = 0; i < barFactors.size(); i++) {
             LegendEntry entry = new LegendEntry();
             entry.formColor = ColorTemplate.MATERIAL_COLORS[i];
             entry.label = barFactors.get(i);
             lentries.add(entry);
         }
+
         l.setXEntrySpace(5f); // set the space between the legend entries on the x-axis
         l.setYEntrySpace(5f);
         l.setCustom(lentries);
 
-
-
         return view;
-       // return inflater.inflate(R.layout.fragment_track_c_o2_data, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
     }
 
 }
--- app/src/main/java/Fragments/TrackHumidityData.java
+++ app/src/main/java/Fragments/TrackHumidityData.java
@@ -1,5 +1,7 @@
 package Fragments;
 
+
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,59 +10,120 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.and.sauna.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TrackHumidityData#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.and.sauna.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TrackHumidityData extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    LineChart lineChart;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TrackHumidityData() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TrackHumidityData.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TrackHumidityData newInstance(String param1, String param2) {
-        TrackHumidityData fragment = new TrackHumidityData();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track_humidity_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_track_humidity_data, container, false);
+
+        lineChart = view.findViewById(R.id.line);
+
+        LineDataSet dataSet = new LineDataSet(dataValues(), "Current Humidity");
+        LineDataSet dataSet2 = new LineDataSet(dataValuesRec(), "Recommended Humidity");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet);
+        dataSets.add(dataSet2);
+
+        //Setting the color of the lines and the width of the highlighted values
+        dataSet.setColors(Color.rgb(241, 196, 15));
+        dataSet2.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataSet.setValueTextColor(Color.BLUE);
+        dataSet2.setValueTextColor(Color.BLUE);
+        dataSet.setValueTextSize(14f);
+        dataSet2.setValueTextSize(14f);
+
+        //Setting line width and other custom settings
+        dataSet.setLineWidth(4);
+        dataSet2.setLineWidth(4);
+        dataSet.setDrawCircles(true);
+        dataSet2.setDrawCircles(true);
+
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+
+        lineChart.setDrawGridBackground(true);
+
+        //Setting border width and color
+        lineChart.setBorderWidth(1);
+        lineChart.setDrawBorders(true);
+
+        //Setting the bottom right corner text
+        Description description = new Description();
+        description.setText("All values in percentages");
+        description.setTextSize(15);
+        lineChart.setDescription(description);
+
+
+        ArrayList<String> lineFactors = new ArrayList<>();
+        lineFactors.add("Recommended");
+        lineFactors.add("Current");
+
+        //Setting up the legend
+        Legend l = lineChart.getLegend();
+        l.setFormSize(10f); // set the size of the legend forms/shapes
+        l.setForm(Legend.LegendForm.CIRCLE); // set what type of form/shape should be used
+        l.setTextSize(12f);
+        l.setTextColor(Color.BLACK);
+        List<LegendEntry> lentries = new ArrayList<>();
+
+        for (int i = 0; i < lineFactors.size(); i++) {
+            LegendEntry entry = new LegendEntry();
+            entry.formColor = ColorTemplate.MATERIAL_COLORS[i];
+            entry.label = lineFactors.get(i);
+            lentries.add(entry);
+        }
+
+        l.setXEntrySpace(5f); // set the space between the legend entries on the x-axis
+        l.setYEntrySpace(5f);
+        l.setCustom(lentries);
+
+        //lineChart.notifyDataSetChanged();
+
+        return view;
     }
+
+    private ArrayList<Entry> dataValues()
+    {
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        dataVals.add(new Entry(0, 20));
+        dataVals.add(new Entry(1, 24));
+        dataVals.add(new Entry(2, 2));
+        dataVals.add(new Entry(3, 18));
+
+        return dataVals;
+    }
+
+    private ArrayList<Entry> dataValuesRec()
+    {
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        dataVals.add(new Entry(0, 5));
+        dataVals.add(new Entry(2, 10));
+
+        return dataVals;
+    }
+
 }
